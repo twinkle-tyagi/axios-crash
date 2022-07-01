7eede688d7c902124e65dcc96ca5c7ec0089ab17
twinkle-tyagi <76639382+twinkle-tyagi@users.noreply.github.com>
2022-07-01 11:21:39 +0530

    MinMaxElements

--- MinMax.js
+++ MinMax.js
@@ -1,17 +1,27 @@
 function min(root)
 {
+    if(root == null)
+    {
+        return null;
+    }
+
     while(root.left !== null)
     {
         root = root.left;
     }
-    return root;
+    return root.data;
 }
 
 function max(root)
 {
+    if(root == null)
+    {
+        return null;
+    }
+
     while(root.right !== null)
     {
         root = root.right;
     }
-    return root;
+    return root.data;
 }
